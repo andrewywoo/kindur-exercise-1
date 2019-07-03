df140f49b94ccc5c0d9623548cc3bbeea531d812
Andrew Woo <andrew.woo@verizon.net>
2019-07-02 22:19:36 -0400

    editing server.js

--- server.js
+++ server.js
@@ -1,4 +1,8 @@
 //server.js
 const app = require("./app");
 
-app.listen(8080);
+// Initialize the app.
+var server = app.listen(process.env.PORT || 8080, function() {
+  var port = server.address().port;
+  console.log("App now running on port", port);
+});
